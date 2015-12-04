ec35a6cab3a28fbab46c2f1452adce3cbab2a8ad
Mikk Andresen <mikk.andresen@gmail.com>
2015-12-04 17:12:02 +0200

    First somewhat useful version. See the README.md for configuration

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # ep_webhooks
 Webhooks for Etherpad Lite.
+
+# WORK IN PROGRESS
+
+While it's somewhat useful, work is in progress. See the **TODO** below. Any ideas and contributions are welcome, but see the **TODO** section first.
+
+# Configuration
+
+In your Etherpad Lite ``settings.json``:
+
+```
+  "ep_webhooks": {
+    "pads": {
+        "update": ["http://dev.toru.ee:3000/api/internal/notifications/pads/update"]
+    }
+  }
+```
+
+* Where the ``pads.update`` configures webhook for ``pads.update`` event. See the "Supported hooks" section.
+* You can specify multiple urls, each of them will be called on the event.
+
+# Supported hooks
+
+## pads.update
+
+Hook is called when any of the pads is updated. Payload contains the id-s of the pads that have been modified.
+ 
+Sample HTTP request made by ep_webhooks to the registered hook url:
+
+```
+POST https://mydomain.com/api/notifications/pads/update
+{
+    padIds: ['pad1', 'pad2', 'pad3']
+}
+```
+
+# TODO
+
+* Create API to register and de-register hooks instead of EP ``settings.json`` configuration.
+    * Require EP APIKEY to authorize registration/de-registration.
+    * Register must accept all parameters for the ``lodash.debounce`` ([https://lodash.com/docs#debounce]()). Right now the configuration is hardcoded. See ``serverHooks.js``.
+    * API should do a pingback on registering to verify that the API is available.
+    * Some kind of mechanism of verifying payloads. May want to borrow good ideas from GitHub [https://developer.github.com/webhooks/]() & [https://developer.github.com/v3/repos/hooks/#pubsubhubbub]().
+* Back-off mechanism on errors - if a hook call fails too much, just stop sending or retry, but increase retry delay with each failure.
+* Define method for telling ``ep_webhooks`` to temporarily delay calling registered hook.
+    * Maybe overkill? You can register/de-register if you want to. 
+    * For example response headers or HTTP response code. Useful when your system is under heavy load and you want to temporarily delay messages for a longer period of time.
+  
---
+++ serverHooks.js
@@ -0,0 +1,93 @@
+var PLUGIN_NAME = 'ep_webhooks';
+
+var _ = require('lodash');
+var request = require('superagent');
+
+var logger = require('ep_etherpad-lite/node_modules/log4js').getLogger(PLUGIN_NAME);
+var padMessageHandler = require('ep_etherpad-lite/node/handler/PadMessageHandler.js');
+
+var pluginSettings; // set with loadSettings hook
+var changedPads = {}; // Pads that have changed. key = padId, value = 1
+
+/**
+ * Call the webhooks
+ *
+ * The ``debounce`` is used to delay execution.
+ *
+ * @see {@link https://lodash.com/docs#debounce}
+ */
+var callPadUpdateWebhooks = _.debounce(function () {
+    logger.debug('callPadUpdateWebhooks', changedPads);
+
+    // No pads changed
+    if (!Object.keys(changedPads).length) {
+        return;
+    }
+
+    var changedPadIds = Object.keys(changedPads);
+    changedPads = {};
+
+    logger.debug('pluginSettings', pluginSettings);
+    var updateHooksToCall = _.get(pluginSettings, ['pads', 'update']);
+
+    if (Array.isArray(updateHooksToCall)) {
+        // Fire and forget - no guarantees of delivery for now
+        updateHooksToCall.forEach(function (path) {
+            request
+                .post(path)
+                .send({padIds: changedPadIds})
+                .end(function (err, res) {
+                    if (err) {
+                        logger.error('callPadUpdateWebhooks - HTTP POST failed to ', path, '. Error was', err);
+                    }
+                });
+        });
+    }
+}, 1000, {maxWait: 5000});
+
+/**
+ * handleMessage hook
+ *
+ * @param {string} hook_name
+ * @param {object} context
+ * @param {Function} cb Callback
+ *
+ * @see {@link http://etherpad.org/doc/v1.5.7/#index_handlemessage}
+ */
+exports.handleMessage = function (hook_name, context, cb) {
+    logger.debug('handleMessage');
+
+    if (pluginSettings) {
+        var messageType = _.get(context.message, 'data.type');
+
+        if (messageType === 'USER_CHANGES') {
+            var padId = _.get(padMessageHandler.sessioninfos[context.client.id], 'padId');
+            if (padId) {
+                logger.debug('handleMessage', 'PAD CHANGED', padId);
+                changedPads[padId] = 1; // Use object, as then I don't need to worry about duplicates
+                callPadUpdateWebhooks();
+            } else {
+                logger.warn('handleMessage', 'Pad changed, but no padId!');
+            }
+        }
+    }
+
+    return cb([context.message]);
+};
+
+/**
+ * loadSettings
+ *
+ * @param {string} hook_name "loadSettings"
+ * @param {object} args Object {settings: {object}}
+ *
+ * @see {@link http://etherpad.org/doc/v1.5.7/#index_loadsettings}
+ */
+exports.loadSettings = function (hook_name, args) {
+    var settings = args.settings;
+    if (settings && settings[PLUGIN_NAME]) {
+        pluginSettings = settings[PLUGIN_NAME];
+    } else {
+        logger.warn('Plugin configuration not found, doing nothing.')
+    }
+};
